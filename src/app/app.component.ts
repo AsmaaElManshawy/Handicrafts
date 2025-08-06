513efb3e869ae7f74fe931e50eefd8602d40ba4e
sama <sama.haitham2004@gmail.com>
2025-08-06 04:13:32 +0300

    Started home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,13 @@ import { FooterComponent } from "./components/footer/footer.component";
 })
 export class AppComponent {
   title = 'shopping';
+  isHomeRoute = false;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.isHomeRoute = event.urlAfterRedirects === '/home';
+    });
+  }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CardHomeComponent } from "../card-home/card-home.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardHomeComponent , CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  products : any[] = [
+    {name : "daisy pottery" , category:"Painted handcraft", desc: "insbired pottery by daisy flowers and pastel colors" , img:"../../../assets/imgs/New folder/daisy pottery.jpg"},
+    {name : "Macrame Mirror" ,category:"Home decor", desc: "DIY Partial Macrame BOHO mirror" , img:"../../../assets/imgs/New folder/mirror.jpg"},
+    {name : "Sunshine mug" , category:"Painted handcraft", desc: "Painted sunshine mug inspired by summer season" , img:"../../../assets/imgs/New folder/sunshine mug.jpg"},
+    {name : "Matriyel wall art" , category:"Home decor", desc: "insbired wall art with boho style and wheat shape" , img:"../../../assets/imgs/New folder/tablu.jpg"},
+    {name : "ring dish" , category:"Painted handcraft", desc: "Painted Flowry ring dishes inspired bysummer season" , img:"../../../assets/imgs/New folder/rings.jpg"}
+  ]
 }
