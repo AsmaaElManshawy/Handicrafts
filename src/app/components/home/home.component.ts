3b677a600279f6febe0460d87714dc5f80c6c303
sama <sama.haitham2004@gmail.com>
2025-08-07 03:16:02 +0300

    Started home & product updated done and card also

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,10 +11,10 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
   products : any[] = [
-    {name : "daisy pottery" , category:"Painted handcraft", desc: "insbired pottery by daisy flowers and pastel colors" , img:"../../../assets/imgs/New folder/daisy pottery.jpg"},
-    {name : "Macrame Mirror" ,category:"Home decor", desc: "DIY Partial Macrame BOHO mirror" , img:"../../../assets/imgs/New folder/mirror.jpg"},
-    {name : "Sunshine mug" , category:"Painted handcraft", desc: "Painted sunshine mug inspired by summer season" , img:"../../../assets/imgs/New folder/sunshine mug.jpg"},
-    {name : "Matriyel wall art" , category:"Home decor", desc: "insbired wall art with boho style and wheat shape" , img:"../../../assets/imgs/New folder/tablu.jpg"},
-    {name : "ring dish" , category:"Painted handcraft", desc: "Painted Flowry ring dishes inspired bysummer season" , img:"../../../assets/imgs/New folder/rings.jpg"}
+    {name : "daisy pottery" , category:"Painted handcraft", desc: "insbired pottery by daisy flowers and pastel colors" , img:"../../../assets/imgs/card-home/daisy pottery.jpg"},
+    {name : "Macrame Mirror" ,category:"Home decor", desc: "DIY Partial Macrame BOHO mirror" , img:"../../../assets/imgs/card-home/mirror.jpg"},
+    {name : "Sunshine mug" , category:"Painted handcraft", desc: "Painted sunshine mug inspired by summer season" , img:"../../../assets/imgs/card-home/sunshine mug.jpg"},
+    {name : "Matriyel wall art" , category:"Home decor", desc: "insbired wall art with boho style and wheat shape" , img:"../../../assets/imgs/card-home/tablu.jpg"},
+    {name : "ring dish" , category:"Painted handcraft", desc: "Painted Flowry ring dishes inspired bysummer season" , img:"../../../assets/imgs/card-home/rings.jpg"}
   ]
 }
